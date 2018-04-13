3a7e4cccf0153e7dfaf0bd9a0462e1e1ad8ba6df
sirgrimorum <andres.espinosa@grimorum.com>
2018-04-13 17:26:06 -0500

    remove csfr protection

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::group(['prefix' => config("sirgrimorum.paymentpass.route_prefix"), 'middleware' => ['web'], 'as' => "paymentpass::"], function () {
+Route::group(['prefix' => config("sirgrimorum.paymentpass.route_prefix"), 'as' => "paymentpass::"], function () {
     Route::any('/{service}/{responseType}', '\Sirgrimorum\PaymentPass\PaymentPassHandler@handleResponse')->name('response');
 });
