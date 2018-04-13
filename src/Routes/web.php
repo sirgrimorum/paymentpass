c233791b79d78f8a2655f961a086654a6b96e9bb
sirgrimorum <andres.espinosa@grimorum.com>
2018-04-13 18:08:06 -0500

    Add session suport

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
-Route::group(['prefix' => config("sirgrimorum.paymentpass.route_prefix"), 'as' => "paymentpass::"], function () {
+Route::group(['prefix' => config("sirgrimorum.paymentpass.route_prefix"), 'middleware' => [
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    ], 'as' => "paymentpass::"], function () {
     Route::any('/{service}/{responseType}', '\Sirgrimorum\PaymentPass\PaymentPassHandler@handleResponse')->name('response');
 });
