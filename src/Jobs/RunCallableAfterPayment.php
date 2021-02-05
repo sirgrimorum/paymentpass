507e3538ed12827580e732e3d267bf216b7c2c1c
SirGrimorum <andres.espinosa@grimorum.com>
2021-02-05 06:33:14 -0500

    callbacks from webhooks could be queued

---
+++ src/Jobs/RunCallableAfterPayment.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Sirgrimorum\PaymentPass\Jobs;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Arr;
+use Sirgrimorum\PaymentPass\Models\PaymentPass;
+use Sirgrimorum\PaymentPass\PaymentPassHandler;
+
+class RunCallableAfterPayment implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $paymentpass;
+
+    protected $state;
+    protected $service;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($state, $service, PaymentPass $paymentpass)
+    {
+        $this->paymentpass = $paymentpass;
+        $this->state = $state;
+        $this->service = $service;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (in_array($this->service, config("sirgrimorum.paymentpass.available_services"))) {
+            $curConfig = (new PaymentPassHandler($this->service))->config;
+            $callbackFunc = Arr::get($curConfig, "service.callbacks.{$this->state}", Arr::get($curConfig, "service.callbacks.other", ""));
+            if (is_callable($callbackFunc)) {
+                call_user_func($callbackFunc, $this->payment);
+            }
+        }
+    }
+}
